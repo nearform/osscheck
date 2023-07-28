4c711adfb79d1980e7e6c9a3eb2c880b1d89264d
Shaun Baker <shaun@schb.co.uk>
2023-07-28 11:54:54 +0100

    chore(#9): ui deployment (#30)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
