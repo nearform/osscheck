6d332931737df678974e742f9176eb86b6e1ce84
Shaun Baker <shaun@schb.co.uk>
2023-07-28 12:00:27 +0100

    chore(#9): ui deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/osshealth/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
