8bfe5df467376742c2f6d32f9107f08133064d21
Shaun Baker <shaun@schb.co.uk>
2023-09-05 11:22:19 +0100

    chore: rename repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/osshealth/',
+  base: '/osscheck/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
